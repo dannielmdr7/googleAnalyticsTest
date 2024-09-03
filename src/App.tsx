8a307ba11e1923ce8ec5f0fa0b91dc3025fcafaf
Daniel Montenegro <dannielmdr7@gmail.com>
2024-09-03 16:51:43 -0500

    feature:analytics agregado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+import ReactGA from 'react-ga4';
+ReactGA.initialize('G-R2R4010YVC')
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log('Daniel emitiendo pageview');
+
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
+  const handleClick1 = () => {
+    console.log('Daniel emitiendo evento 1');
+    
+    ReactGA.event({
+      category: 'User 1',
+      action: 'Clicked a button'
+    });
+  }
+  const handleClick2 = () => {
+    console.log('Daniel emitiendo evento 2');
+    ReactGA.event({
+      category: 'User 2',
+      action: 'Clicked a button'
+    });
+  }
+  const handleClick3 = () => {
+    console.log('Daniel emitiendo evento 3');
+    ReactGA.event({
+      category: 'User 3',
+      action: 'Clicked a button'
+    });
+  }
+
 
   return (
     <>
@@ -18,8 +50,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => handleClick1()}>
+          click en 1
+        </button>
+        <button onClick={() => handleClick2()}>
+          click en 2
+        </button>
+        <button onClick={() => handleClick3()}>
+          click en 3
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
